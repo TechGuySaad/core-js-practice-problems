7097176369597bacfeb4ad369c90e0ce32647769
Saad Rahman <saadreh5@gmail.com>
2025-03-02 18:01:28 +0500

    done with function problems

---
+++ functions/Question11.js
@@ -0,0 +1,9 @@
+// Convert the function into arrow function
+
+function add(a, b) {
+  return a + b;
+}
+
+const Add = (a, b) => {
+  a + b;
+};
---
+++ functions/Question12.js
@@ -0,0 +1,9 @@
+// What happens when a function doesnt have a return statement
+
+function add(a, b) {
+  a + b;
+}
+
+console.log(add(8, 8));
+
+// When a function doesnt have a return statement it just returns without a value to the place where the function was called when function body ends

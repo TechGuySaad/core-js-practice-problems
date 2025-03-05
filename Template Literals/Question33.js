5ee213f1056de6b4e7a81e6671d4a4f6be390582
Saad Rahman <saadreh5@gmail.com>
2025-03-05 22:54:01 +0500

    done with template literals

---
+++ Template Literals/Question33.js
@@ -0,0 +1,5 @@
+let name = "John";
+let age = 30;
+
+console.log("My name is" + name + " and I am " + age + "years old.");
+console.log(`My name is ${name} and I am ${age} years old.`);
---
+++ Template Literals/Question34.js
@@ -0,0 +1,7 @@
+// what will be logged
+const a = 10;
+const b = 20;
+
+console.log(`The sum is ${a + b}`);
+
+// expected output: the sum is 30
